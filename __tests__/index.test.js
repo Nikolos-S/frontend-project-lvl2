71ffc96905e5aa09f5fd8a7aec367d821b6d11c8
Nikolos-S <sarkozzy@mail.ru>
2023-03-02 21:26:42 +0700

    add tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -8,11 +8,27 @@ const __dirname = dirname(__filename);
 const getFixturePath = (filename) => join(__dirname, '..', '__fixtures__', filename);
 const readFixture = (file) => readFileSync(getFixturePath(file), 'utf8');
 
+const stylishResult1 = readFixture('expected1.txt');
+const plainResult1 = readFixture('expectedPlain1.txt');
+const jsonResult1 = readFixture('expectedJson1.txt');
+
 const stylishResult = readFixture('expected.txt');
 const plainResult = readFixture('expectedPlain.txt');
 const jsonResult = readFixture('expectedJson.txt');
 
-describe('Gendiff_test', () => {
+describe('Gendiff_test1', () => {
+  test.each(['json', 'yml'])('calculate differences', (extenshion) => {
+    const filepath1 = getFixturePath(`file3.${extenshion}`);
+    const filepath2 = getFixturePath(`file4.${extenshion}`);
+
+    expect(genDiff(filepath1, filepath2)).toEqual(stylishResult1);
+    expect(genDiff(filepath1, filepath2, 'stylish')).toEqual(stylishResult1);
+    expect(genDiff(filepath1, filepath2, 'plain')).toEqual(plainResult1);
+    expect(genDiff(filepath1, filepath2, 'json')).toEqual(jsonResult1);
+  });
+});
+
+describe('Gendiff_test2', () => {
   test.each(['json', 'yml'])('calculate differences', (extenshion) => {
     const filepath1 = getFixturePath(`file1.${extenshion}`);
     const filepath2 = getFixturePath(`file2.${extenshion}`);
